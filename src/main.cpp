2ca63b0e90c3c305b91b4a843955e97efc2ff316
Murillo Santana <mvbentosantana@gmail.com>
2024-06-25 13:43:46 +0200

    added makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,24 @@
+//defines
 #define GLFW_INCLUDE_VULKAN
+
+//glfw
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
-#include <iostream>
-
-int main()
+int main(void)
 {
+	//setup
 	glfwInit();
-
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-
-	uint32_t extensionCount = 0;
-	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-	std::cout << extensionCount << " extensions supported\n";
-
-	glm::vec4 vec;
-	glm::mat4 matrix;
-	glm::vec4 test = matrix * vec;
-
+	//main loop
 	while(!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 	}
-
+	//window
 	glfwDestroyWindow(window);
-
+	//finish
 	glfwTerminate();
-
+	//return
 	return 0;
 }
