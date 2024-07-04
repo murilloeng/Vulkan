2cb6f935f4278d111fdd787834213f374654511b
Murillo Vinicius Bento Santana <mvbentosantana@gmail.com>
2024-07-04 13:27:22 +0200

    some pogress

--- inc/Application.hpp
+++ inc/Application.hpp
@@ -9,6 +9,13 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
+struct queue_family_indices
+{
+	std::optional<uint32_t> m_family_graphics;
+	std::optional<uint32_t> m_family_presentation;
+};
+
+
 class Application
 {
 public:
@@ -30,16 +37,33 @@ private:
 	void cleanup(void);
 
 	//create
+	void create_surface(void);
 	void create_instance(void);
+	void create_logical_device(void);
 	void create_physical_device(void);
 
 	//physical device
 	bool check_physical_device(VkPhysicalDevice);
+	bool check_extension_support(VkPhysicalDevice);
+	queue_family_indices find_queue_families(VkPhysicalDevice);
 
 	//data
 	unsigned m_width;
 	unsigned m_height;
+
+	//instances
 	GLFWwindow* m_window;
 	VkInstance m_instance;
+	VkSurfaceKHR m_surface;
+
+	//queues
+	VkQueue m_queue_graphics;
+	VkQueue m_queue_presentation;
+
+	//devices
+	VkDevice m_device;
 	VkPhysicalDevice m_physical_device;
+
+	//extensions
+	static const std::vector<const char*> m_extensions;
 };
--- src/Application.cpp
+++ src/Application.cpp
@@ -40,7 +40,9 @@ void Application::setup_glfw(void)
 void Application::setup_vulkan(void)
 {
 	create_instance();
+	create_surface();
 	create_physical_device();
+	create_logical_device();
 }
 
 //loop
@@ -55,6 +57,10 @@ void Application::main_loop(void)
 //cleanup
 void Application::cleanup(void)
 {
+	//device
+	vkDestroyDevice(m_device, nullptr);
+	//surface
+	vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
 	//instance
 	vkDestroyInstance(m_instance, nullptr);
 	//window
@@ -64,11 +70,18 @@ void Application::cleanup(void)
 }
 
 //create
+void Application::create_surface(void)
+{
+	if(glfwCreateWindowSurface(m_instance, m_window, nullptr, &m_surface) != VK_SUCCESS)
+	{
+		throw std::runtime_error("Failed to create window surface!");
+	}
+}
 void Application::create_instance(void)
 {
 	//data
 	VkApplicationInfo app_info;
-	VkInstanceCreateInfo instance_info;
+	VkInstanceCreateInfo instance_create_info{};
 	//extensions
 	uint32_t glfw_extension_count = 0;
 	const char** glfw_extensions = glfwGetRequiredInstanceExtensions(&glfw_extension_count);
@@ -81,17 +94,51 @@ void Application::create_instance(void)
 	app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	app_info.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
 	//instance
-	instance_info.pNext = nullptr;
-	instance_info.enabledLayerCount = 0;
-	instance_info.pApplicationInfo = &app_info;
-	instance_info.ppEnabledExtensionNames = extensions.data();
-	instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-	instance_info.enabledExtensionCount = (uint32_t) extensions.size();
+	instance_create_info.pNext = nullptr;
+	instance_create_info.enabledLayerCount = 0;
+	instance_create_info.pApplicationInfo = &app_info;
+	instance_create_info.ppEnabledExtensionNames = extensions.data();
+	instance_create_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+	instance_create_info.enabledExtensionCount = (uint32_t) extensions.size();
 	//create
-	if(vkCreateInstance(&instance_info, nullptr, &m_instance) != VK_SUCCESS)
+	if(vkCreateInstance(&instance_create_info, nullptr, &m_instance) != VK_SUCCESS)
 	{
 		throw std::runtime_error("Failed to create instance!");
 	}
+}
+void Application::create_logical_device(void)
+{
+	//data
+	float queue_priority = 1.0f;
+	VkDeviceCreateInfo device_create_info{};
+	VkPhysicalDeviceFeatures physiscal_device_features{};
+	std::vector<VkDeviceQueueCreateInfo> queue_create_infos(2);
+	queue_family_indices indices = find_queue_families(m_physical_device);
+	//queue info
+	queue_create_infos[0].queueCount = 1;
+	queue_create_infos[1].queueCount = 1;
+	queue_create_infos[0].pQueuePriorities = &queue_priority;
+	queue_create_infos[1].pQueuePriorities = &queue_priority;
+	queue_create_infos[0].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue_create_infos[1].sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queue_create_infos[0].queueFamilyIndex = indices.m_family_graphics.value();
+	queue_create_infos[1].queueFamilyIndex = indices.m_family_presentation.value();
+	//device info
+	device_create_info.enabledLayerCount = 0;
+	device_create_info.queueCreateInfoCount = 2;
+	device_create_info.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	device_create_info.pQueueCreateInfos = queue_create_infos.data();
+	device_create_info.pEnabledFeatures = &physiscal_device_features;
+	device_create_info.ppEnabledExtensionNames = m_extensions.data();
+	device_create_info.enabledExtensionCount = (uint32_t) m_extensions.size();
+	//create
+	if(vkCreateDevice(m_physical_device, &device_create_info, nullptr, &m_device) != VK_SUCCESS)
+	{
+		throw std::runtime_error("Failed to create logical device!");
+	}
+	//queue
+	vkGetDeviceQueue(m_device, indices.m_family_graphics.value(), 0, &m_queue_graphics);
+	vkGetDeviceQueue(m_device, indices.m_family_presentation.value(), 0, &m_queue_presentation);
 }
 void Application::create_physical_device(void)
 {
@@ -123,7 +170,61 @@ void Application::create_physical_device(void)
 }
 
 //physical device
-bool Application::check_physical_device(VkPhysicalDevice device)
+bool Application::check_physical_device(VkPhysicalDevice device) 
 {
-	return true;
+	//data
+	queue_family_indices indices = find_queue_families(device);
+	//return
+	return 
+		indices.m_family_graphics.has_value() && 
+		indices.m_family_presentation.has_value() &&
+		check_extension_support(device);
 }
+bool Application::check_extension_support(VkPhysicalDevice device)
+{
+	//count
+	uint32_t extension_count;
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extension_count, nullptr);
+	//extensions
+	std::vector<VkExtensionProperties> available_extensions(extension_count);
+	std::set<std::string> required_extensions(m_extensions.begin(), m_extensions.end());
+	vkEnumerateDeviceExtensionProperties(device, nullptr, &extension_count, available_extensions.data());
+	//check
+	for(const VkExtensionProperties& extension : available_extensions)
+	{
+		required_extensions.erase(extension.extensionName);
+	}
+	//return
+	return required_extensions.empty();
+}
+queue_family_indices Application::find_queue_families(VkPhysicalDevice device)
+{
+	//count
+	uint32_t queue_family_count = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, nullptr);
+	//families
+	std::vector<VkQueueFamilyProperties> queue_families(queue_family_count);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, queue_families.data());
+	//search
+	uint32_t index = 0;
+	queue_family_indices indices;
+	VkBool32 presentation_support = false;
+	for(const VkQueueFamilyProperties& queue_family : queue_families)
+	{
+		if(queue_family.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+		{
+			indices.m_family_graphics = index;
+		}
+		vkGetPhysicalDeviceSurfaceSupportKHR(device, index, m_surface, &presentation_support);
+		if(presentation_support)
+		{
+			indices.m_family_presentation = index;
+		}
+		index++;
+	}
+	//return
+	return indices;
+}
+
+//static data
+const std::vector<const char*> Application::m_extensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
