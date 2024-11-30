720e360300c350d5bc02878c105bf73740560325
Murillo Vinicius Bento Santana <mvbentosantana@gmail.com>
2024-11-30 15:28:40 +0100

    restart

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
+//std
+#include <cstdlib>
+
 //vulkan
-#include "Vulkan/inc/AppGalea.hpp"
 #include "Vulkan/inc/Application.hpp"
 
 int main(void)
 {
 	//application
-	Application app;
-	try { app.run(); }
-	catch (const std::exception& exception) 
-	{
-		fprintf(stderr, "%s\n", exception.what());
-		return EXIT_FAILURE;
-	}
+	// Application app;
+	// try { app.run(); }
+	// catch (const std::exception& exception) 
+	// {
+	// 	fprintf(stderr, "%s\n", exception.what());
+	// 	return EXIT_FAILURE;
+	// }
 	//retun
 	return EXIT_SUCCESS;
 }
